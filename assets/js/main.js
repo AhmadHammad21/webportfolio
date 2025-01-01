fcf7335855f6859eda1a3b6834c4c40631539cd1
khaledjafar <khaledjafarmoh@gmail.com>
2025-01-01 22:31:36 +0300

    external-link

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,3 +256,9 @@
 		}
 
 })(jQuery);
+
+document.querySelectorAll('.external-link').forEach(link => {
+	link.setAttribute('target', '_blank');
+	link.setAttribute('rel', 'noopener noreferrer');
+  });
+  
